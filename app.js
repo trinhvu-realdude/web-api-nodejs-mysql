b2f94766cda602776d8ddd4cc131036cf22d0a52
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-05-18 00:10:35 +0700

    first commit

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ const port = process.env.PORT || 3000;
 
 app.use('/users', userRouter);
 
-app.engine('html', require('ejs').renderFile);
 app.set('views', (path.join(__dirname, '/views')));
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(cookieParser());
