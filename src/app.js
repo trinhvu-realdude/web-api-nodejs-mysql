aa5c762646cc59ee323ce3c4811371fcbfe1e0e4
trinhvu-realdude <vudangtrinh29102000@gmail.com>
2021-06-30 15:16:19 +0700

    test upload multer

--- src/app.js
+++ src/app.js
@@ -2,7 +2,18 @@ const expressSession = require('express-session')
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
+
+const multer = require("multer");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './src/uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+const upload = multer({storage: storage});
 
 const passport = require("passport");
 require("./controllers/passport")(passport);
@@ -17,7 +28,6 @@ const corsOptions = {
 app.set('views', (path.join(__dirname, '/views')));
 app.set('view engine', 'ejs');
 
-app.use(expressSession({secret: 'keyboard-cat', resave: false, saveUninitialized: false}));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,6 +35,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressSession({secret: 'keyboard-cat', resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.post('/upload', upload.single('file'), function(req, res) {
+    console.log(req.file);
+    res.send({
+        message: "Upload successfully"
+    })
+});
+
+app.get('/', (req, res) => {
+    res.render('upload');
+})
 
 require("./routes/user.route")(app);
 require("./routes/role.route")(app);
--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -5,7 +5,6 @@ const userService = require("../services/user.service");
 exports.authenticateRole = (roleArray) => async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        console.log(`Token: ${token}`);
         const decoded = jwt.verify(token, config.app.secretKey);
         console.log(decoded);
         if (decoded) {
@@ -14,7 +13,7 @@ exports.authenticateRole = (roleArray) => async (req, res, next) => {
             req.email = decoded.email;
 
             const role = await userService.getRoleUser(decoded.roleId);
-            console.log(role.name);
+
             const authorized = roleArray.indexOf(role.name);
 
             if (authorized === 0 || authorized === 1) {
